83706c7f660715858f0c07b115fd2ab1f0ef8c27
sanjuli14 <juliocesargarciagarcia395@gmail.com>
2026-04-29 19:46:30 -0400

    ya no se que hacer

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  openrouterApiKey: 'PLACEHOLDER_REPLACE_ME',
+  openrouterApiKey:
+    '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',
   defaultPlayerName: 'Streamer',
 };
