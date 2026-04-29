aadc606345c8d6e5e727488f759a405823b45472
sanjuli14 <juliocesargarciagarcia395@gmail.com>
2026-04-29 19:11:48 -0400

    ...

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  openrouterApiKey:
-    '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',
-  defaultPlayerName: 'Streamer',
-};
