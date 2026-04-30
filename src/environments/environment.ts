959fff5185396e8d38c7f5c1e319899faba59e9f
sanjuli14 <juliocesargarciagarcia395@gmail.com>
2026-04-29 20:04:39 -0400

    ultimo intento

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   openrouterApiKey:
-    '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',
+    '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',
   defaultPlayerName: 'Streamer',
 };
